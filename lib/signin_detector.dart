e2f7569720f3df02da7428ed303441df5b1f4884
Benz Chanyanuch Somprakit <102774007+csomprakit@users.noreply.github.com>
2024-03-02 20:49:15 -0800

    added signin_detector.dart

---
+++ lib/signin_detector.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'sign_in_up_form.dart';
+import 'leaderboard_page.dart';
+
+class SignInDetector extends StatelessWidget {
+  const SignInDetector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot)
+      {
+      print("Auth state change: $snapshot");
+      if (snapshot.connectionState == ConnectionState.waiting) {
+      return CircularProgressIndicator();
+      }
+
+      if (snapshot.hasData && snapshot.data != null) {
+      print("User signed in. Email: ${snapshot.data!.email}");
+      return LeaderboardPage();
+        //Text("Signed In as ${snapshot.data!.email}");
+      }
+
+      print("User not signed in. Showing SignInUpForm.");
+      return const SignInUpForm();
+      },
+    );
+  }
+}
