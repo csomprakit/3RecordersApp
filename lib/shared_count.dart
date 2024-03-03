9bc01db6295b96434f38066ac12c77976ebcdcfb
Benz Chanyanuch Somprakit <102774007+csomprakit@users.noreply.github.com>
2024-03-03 03:57:05 -0800

    updated shared_count.dart

--- lib/shared_count.dart
+++ lib/shared_count.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SharedCount with ChangeNotifier {
   int _count;
@@ -8,21 +10,31 @@ class SharedCount with ChangeNotifier {
 
   SharedCount(this._count, this._date, this._dedicationLevel, this._category);
 
-  int getCount() { return _count; }
-  DateTime getDate(){return _date;}
-  int getDedicationLevel(){return _dedicationLevel;}
-  String getCategory(){return _category;}
+  int getCount() {
+    return _count;
+  }
+
+  DateTime getDate() {
+    return _date;
+  }
+
+  int getDedicationLevel() {
+    return _dedicationLevel;
+  }
+
+  String getCategory() {
+    return _category;
+  }
 
   void setCount(int newCount) {
     _count = newCount;
   }
 
-  void setDate(DateTime newDate)
-  {
+  void setDate(DateTime newDate) {
     _date = newDate;
   }
 
-  void setCategory(String newCategory){
+  void setCategory(String newCategory) {
     _category = newCategory;
   }
 
@@ -30,19 +42,13 @@ class SharedCount with ChangeNotifier {
     _dedicationLevel = newDedicationLevel;
   }
 
-  void updateCount(DateTime newDate, String newCategory)
-  {
+  void updateCount(DateTime newDate, String newCategory) {
     int newCount = _count;
-    if(_date.difference(newDate).inHours < 1)
-    {
+    if (_date.difference(newDate).inHours < 1) {
       newCount = newCount + 24;
-    }
-    else if (_date.difference(newDate).inHours >24)
-    {
+    } else if (_date.difference(newDate).inHours > 24) {
       newCount = newCount + 1;
-    }
-    else
-    {
+    } else {
       newCount = newCount + newDate.difference(newDate).inHours;
     }
     _count = newCount;
@@ -52,23 +58,33 @@ class SharedCount with ChangeNotifier {
     notifyListeners();
   }
 
-  void updateDedicationLevel()
-  {
-    if(_count > 5000)
-      {
-        _dedicationLevel = 5;
-      }
-    else if (_count > 1000)
-      {
-        _dedicationLevel = 4;
-      }
-    else if(_count > 500)
-      {
-        _dedicationLevel = 3;
-      }
-    else if(_count > 100)
-      {
-        _dedicationLevel = 2;
-      }
+  void updateDedicationLevel() {
+    if (_count > 5000) {
+      _dedicationLevel = 5;
+    } else if (_count > 1000) {
+      _dedicationLevel = 4;
+    } else if (_count > 500) {
+      _dedicationLevel = 3;
+    } else if (_count > 100) {
+      _dedicationLevel = 2;
+    }
+    saveCountData();
+  }
+
+  Future<void> saveCountData() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    CollectionReference collection = db.collection('sharedcount');
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+    QuerySnapshot snapshot =
+        await collection.where('userID', isEqualTo: userId).get();
+
+    if (snapshot.docs.isNotEmpty) {
+      DocumentSnapshot document = snapshot.docs.first;
+      final id = document.id;
+      collection.doc(id).update({
+        'dedicationLevel': _dedicationLevel,
+        'totalPoints': _count,
+      });
+    }
   }
 }
